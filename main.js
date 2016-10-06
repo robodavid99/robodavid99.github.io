1c637e5873b9e093f2e3fffa668732e1bdba6832
DAvid Romero <robodavid99@gmail.com>
2016-10-06 23:01:40 +0000

    complete button?

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-function sayThatWaseasy(){
+function sayThatWasEasy(){
     var thatWasEasy=new Audio("that_was_easy.mp3");
     thatWasEasy.play();
 }
-$("#easy").on("click", sayThatWasEasy);
+/*global $*/$("#easy").on("click", sayThatWasEasy);
